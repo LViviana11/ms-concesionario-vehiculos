dc5a101d806e7ac0ac1886cf205d1004427d1aea
Leidy Viviana Cuaspud Velasquez <leidy.1701913815@ucaldas.edu.co>
2021-09-12 23:07:08 -0500

    Microservicio para los vehiculos

---
+++ src/models/foto.model.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Vehiculo} from './vehiculo.model';
+
+@model({
+  settings: {
+    foreignKeys: {
+      fk_foto_id_vehiculo: {
+        name: 'fk_foto_id_vehiculo',
+        entity: 'Vehiculo',
+        entityKey: 'id',
+        foreignKey: 'id_vehiculo',
+      }
+    },
+  },
+})
+export class Foto extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @belongsTo(() => Vehiculo, {name: 'vehiculo'})
+  id_vehiculo: number; // es una propiedad con llave foranea que viene de vehículo
+
+  constructor(data?: Partial<Foto>) {
+    super(data);
+  }
+}
+
+export interface FotoRelations {
+  // describe navigational properties here
+}
+
+export type FotoWithRelations = Foto & FotoRelations;
